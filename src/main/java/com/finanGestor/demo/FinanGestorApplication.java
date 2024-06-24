cad75286f64462ee1ddf71c02db266e1fa8b07aa
Gabriel Lopes Gonçalves <gabriellopesgoncalves189@gmail.com>
2024-06-23 21:09:06 -0300

    Primeiro comite

---
+++ src/main/java/com/finanGestor/demo/FinanGestorApplication.java
@@ -0,0 +1,13 @@
+package com.finanGestor.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinanGestorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinanGestorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/finanGestor/demo/FinanGestorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.finanGestor.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinanGestorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
